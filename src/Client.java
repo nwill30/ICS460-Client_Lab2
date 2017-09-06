692c71ccd6bc026bca3e6470ee5af1e5bdecc42a
nwill30 <techexpressnw@gmail.com>
2017-09-06 16:57:08 -0500

    Add Server Chat

--- src/Client.java
+++ src/Client.java
@@ -1,19 +1,43 @@
-import java.io.DataOutputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.ConnectException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
     public static void main(String args[])throws Exception{
 
-        Socket socket = new Socket("127.0.0.1",5000);
-        String message = "Accept Message";
+        Socket socket = null;
+        try {
+            socket = new Socket("127.0.0.1", 5000);
+        }catch (ConnectException e){
+            e.printStackTrace();
+            System.out.println("Err: No connection available at specified location.");
+
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the file directory of a location on the server.");
+        String userInput = scanner.nextLine();
 
         OutputStream outputStream = socket.getOutputStream();
         DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-        dataOutputStream.writeBytes(message);
+        dataOutputStream.writeBytes(userInput);
+
         dataOutputStream.close();
-        outputStream.close();
-        socket.close();
+
+        InputStream inputStream = socket.getInputStream();
+
+        BufferedReader bufferedReader =  new BufferedReader(new InputStreamReader(inputStream));
+
+        String readLine = bufferedReader.readLine();
+
+        System.out.println(readLine);
+
+//        bufferedReader.close();
+//        inputStream.close();
+//        socket.close();
+
     }
 }
